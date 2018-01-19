f07ba849c25ba953f07317bc10d5bad215d1e268
Mehsam Saeed <mehsam@Mehsams-MacBook-Pro.local>
2018-01-19 17:36:27 +0500

    Initial Commit

---
+++ LearnArabic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LearnArabic
+//
+//  Created by Mehsam Saeed on 19/01/2018.
+//  Copyright © 2018 Mehsam Saeed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LearnArabic/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LearnArabic
+//
+//  Created by Mehsam Saeed on 19/01/2018.
+//  Copyright © 2018 Mehsam Saeed. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
